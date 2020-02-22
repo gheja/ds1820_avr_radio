24f3a8b2cdfd3af767b4ac7198b5d47c6f64059d
Gabor Heja <gheja@m00box.git.stdio.hu>
2020-02-22 11:22:43 +0100

    Added fuse values in comment

--- build.sh
+++ build.sh
@@ -16,4 +16,7 @@ avr-gcc -std=c99 -g -Os -mmcu=atmega328p -o ${program}.o -DF_CPU=8000000 \
 avr-objcopy -j .text -j .data -O ihex ${program}.o ${program}.hex || exit 1
 avr-size -d --mcu=atmega328p ${program}.o
 
+# E:07, H:D9, L:E2
+# sudo avrdude -c usbasp -p m328p -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0x07:m
+
 sudo avrdude -c usbasp -p m328p -U flash:w:${program}.hex
